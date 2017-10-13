2bff2c013b1f5b51b550a7dc87a342a926369eef
David Fiedor <davefu@seznam.cz>
2017-10-13 17:15:55 +0200

    Added Ripple tag property for address.

--- src/BitOasis/Coin/Address/RippleAddress.php
+++ src/BitOasis/Coin/Address/RippleAddress.php
@@ -15,19 +15,33 @@ class RippleAddress implements CryptocurrencyAddress {
 	/** @var string */
 	protected $address;
 
+	/** @var int should be unsigned 32b value */
+	protected $tag;
+
 	/** @var Cryptocurrency */
 	protected $currency;
 
-	public function __construct($address, Cryptocurrency $currency) {
+	/**
+	 * @param string $address
+	 * @param Cryptocurrency $currency
+	 * @param $tag
+	 * @throws InvalidAddressException
+	 */
+	public function __construct($address, Cryptocurrency $currency, $tag = null) {
 		if (!$this->isValid($address)) {
 			throw new InvalidAddressException('This is not valid ripple address - ' . $address);
 		}
 		$this->address = $address;
+		$this->tag = $tag == '' || $tag < 0 ? null : (int)$tag;
 		$this->currency = $currency;
 	}
 
 	public function toString() {
-		return $this->address;
+		$value = "Address: $this->address";
+		if ($this->tag !== null) {
+			$value .= ", Tag: $this->tag";
+		}
+		return $value;
 	}
 
 	public function getCurrency() {
@@ -35,15 +49,16 @@ class RippleAddress implements CryptocurrencyAddress {
 	}
 
 	public function serialize() {
-		return $this->address;
+		return $this->address . ($this->tag !== null ? "#$this->tag" : '');
 	}
 
 	public function equals(CryptocurrencyAddress $address) {
-		return $address instanceof static && $this->currency->equals($address->currency) && $this->address === $address->address;
+		return $address instanceof static && $this->currency->equals($address->currency) && $this->address === $address->address && $this->tag === $address->tag;
 	}
 
 	public static function deserialize($string, Cryptocurrency $cryptocurrency) {
-		return new static($string, $cryptocurrency);
+		@list($address, $tag) = explode('#', $string);
+		return new static($address, $cryptocurrency, $tag);
 	}
 
 	private function isValid($address) {
