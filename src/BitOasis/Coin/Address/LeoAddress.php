230d804874f2d5bf8a8f0d768cd78ec3b6b1e1bc
David Fiedor <davefu@seznam.cz>
2020-02-12 11:30:55 +0100

    Fixed LEO address.

--- src/BitOasis/Coin/Address/LeoAddress.php
+++ src/BitOasis/Coin/Address/LeoAddress.php
@@ -5,5 +5,35 @@ namespace BitOasis\Coin\Address;
 /**
  * @author David Fiedor <davefu@seznam.cz>
  */
-class LeoAddress extends EthereumAddress {
+class LeoAddress extends BaseMultiProtocolAddress {
+
+	/**
+	 * @inheritDoc
+	 */
+	public static function supportsClassAdditionalId() {
+		return false;
+	}
+
+	/**
+	 * LEO does not utilize additional ID since Ethereum address
+	 * is the only valid as underlying protocols addresses so far
+	 *
+	 * @inheritDoc
+	 */
+	public static function getClassAdditionalIdName() {
+		return null;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function createUnderlyingProtocolAddress($address, Cryptocurrency $cryptocurrency) {
+		try {
+			$this->cryptocurrencyAddress = new EthereumAddress($address, $cryptocurrency);
+		} catch (InvalidAddressException $e) {
+		}
+		if ($this->cryptocurrencyAddress === null) {
+			throw new InvalidAddressException("$address is not valid/supported Tether address, only ECR20 layer is supported.");
+		}
+	}
 }
--- src/BitOasis/Coin/Address/TetherAddress.php
+++ src/BitOasis/Coin/Address/TetherAddress.php
@@ -3,68 +3,9 @@
 namespace BitOasis\Coin\Address;
 
 use BitOasis\Coin\Cryptocurrency;
-use BitOasis\Coin\CryptocurrencyAddress;
 use BitOasis\Coin\Exception\InvalidAddressException;
 
-class TetherAddress implements CryptocurrencyAddress {
-
-	/** @var CryptocurrencyAddress */
-	protected $cryptocurrencyAddress;
-
-	/** @var Cryptocurrency */
-	protected $currency;
-
-	/**
-	 * @param $address
-	 * @param Cryptocurrency $currency
-	 * @throws InvalidAddressException
-	 */
-	public function __construct($address, Cryptocurrency $currency) {
-		$this->currency = $currency;
-		$this->createUnderlyingProtocolAddress($address, $currency);
-	}
-
-	/**
-	 * @inheritDoc
-	 */
-	public function getAddress() {
-		return $this->cryptocurrencyAddress->getAddress();
-	}
-
-	/**
-	 * @inheritDoc
-	 */
-	public function toString() {
-		return $this->cryptocurrencyAddress->toString();
-	}
-
-	/**
-	 * @inheritDoc
-	 */
-	public function serialize() {
-		return $this->cryptocurrencyAddress->serialize();
-	}
-
-	/**
-	 * @inheritDoc
-	 */
-	public function getCurrency() {
-		return $this->cryptocurrencyAddress->getCurrency();
-	}
-
-	/**
-	 * @inheritDoc
-	 */
-	public function supportsAdditionalId() {
-		return $this->cryptocurrencyAddress->supportsAdditionalId();
-	}
-
-	/**
-	 * @inheritDoc
-	 */
-	public function getAdditionalIdName() {
-		return $this->cryptocurrencyAddress->getAdditionalIdName();
-	}
+class TetherAddress extends BaseMultiProtocolAddress {
 
 	/**
 	 * @inheritDoc
@@ -86,29 +27,6 @@ class TetherAddress implements CryptocurrencyAddress {
 	/**
 	 * @inheritDoc
 	 */
-	public function getAdditionalId() {
-		return $this->cryptocurrencyAddress->getAdditionalId();
-	}
-
-	/**
-	 * @inheritDoc
-	 */
-	public function equals(CryptocurrencyAddress $address) {
-		return $this->cryptocurrencyAddress->equals($address);
-	}
-
-	/**
-	 * @inheritDoc
-	 */
-	public static function deserialize($string, Cryptocurrency $cryptocurrency) {
-		return new static($string, $cryptocurrency);
-	}
-
-	/**
-	 * @param $address
-	 * @param Cryptocurrency $cryptocurrency
-	 * @throws InvalidAddressException
-	 */
 	public function createUnderlyingProtocolAddress($address, Cryptocurrency $cryptocurrency) {
 		try {
 			$this->cryptocurrencyAddress = new EthereumAddress($address, $cryptocurrency);
@@ -118,12 +36,4 @@ class TetherAddress implements CryptocurrencyAddress {
 			throw new InvalidAddressException("$address is not valid/supported Tether address, only ECR20 layer is supported.");
 		}
 	}
-
-	/**
-	 * @return bool
-	 */
-	public function isEthereumAddress() {
-		return $this->cryptocurrencyAddress instanceof EthereumAddress;
-	}
-
 }
