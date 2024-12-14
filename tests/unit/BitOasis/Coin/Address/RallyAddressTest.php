35e20e0d4559751f317daf3be276346696ddd36d
Ahmad Yousef <ahmad.yousef@bitoasis.net>
2024-12-14 16:34:21 +0300

    Add Rally Address Test

---
+++ tests/unit/BitOasis/Coin/Address/RallyAddressTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace unit\BitOasis\Coin\Address;
+
+use BitOasis\Coin\Address\RallyAddress;
+use BitOasis\Coin\Cryptocurrency;
+use BitOasis\Coin\CryptocurrencyNetwork;
+use BitOasis\Coin\Exception\InvalidAddressException;
+use UnitTest;
+use UnitTestUtils;
+
+/**
+ * @author ahmad.yousef <ahmad.yousef@bitoasis.net>
+ */
+class RallyAddressTest extends UnitTest {
+
+	public function providerInvalidAddress(): array {
+		return [
+			['0x6c3e4cb2e96bO1f4b866965a91ed4437839a121a'],
+			['0x6c3e4cb2e96b01f4b866965a91ed4437839a121g'],
+			['tz1fhW886WYc5PQuGu7M3TRjwVTjrtQnKoqM'],
+			['39JXi45Nkgzk8hxz6aHYuefnsDp7qnf4fx'],
+			['44AFFq5kSiGBoZ4NMDwYtN18obc8AemS33DBLWs3H7otXft3XjrpDtQGv7SqSsaBYBb98uNbr2VBBEt7f2wfn3RVGQBEP3A'],
+		];
+	}
+
+	public function providerValidate(): array {
+		return [
+			['0x20C5B10046808c331e7AabF202b0c20Ca526CC0b'],
+			['0xB3ab6e1AE629fe521942d466b089606758a91542'],
+			['0x8D14b4FAE79aA23BA19579761885E1f27B635a20'],
+			['0x0D0707963952f2fBA59dD06f2b425ace40b492Fe'],
+			['0x8a5a589f95B476D35865C5FbD4AE46d8cfD30245'],
+		];
+	}
+
+	/**
+	 * @dataProvider providerInvalidAddress
+	 */
+	public function testInvalidAddress(string $address): void {
+		$this->tester->expectThrowable(InvalidAddressException::class, function () use ($address): void {
+			$this->createAddress($address);
+		});
+	}
+
+	/**
+	 * @throws InvalidAddressException
+	 * @dataProvider providerValidate
+	 */
+	public function testAdditionalId(string $address): void {
+		$createdAddress = $this->createAddress($address);
+		$this->assertFalse($createdAddress->supportsAdditionalId());
+		$this->assertNull($createdAddress->getAdditionalIdName());
+		$this->assertNull($createdAddress->getAdditionalId());
+	}
+
+	/**
+	 * @throws InvalidAddressException
+	 */
+	protected function createAddress(string $address): RallyAddress {
+		return new RallyAddress(
+			$address,
+			UnitTestUtils::getCryptocurrency(Cryptocurrency::PEPE),
+			UnitTestUtils::getCryptocurrencyNetwork(CryptocurrencyNetwork::ETHEREUM)
+		);
+	}
+}
