c26228ce4690bc228cb05d3b506b37f95dc66f9a
Ahmad Yousef <106519736+AhmadEYousef@users.noreply.github.com>
2025-01-07 13:01:07 +0300

    Enforce 0x Prefix for EVM Compatible Addresses (#70)

--- src/BitOasis/Coin/Address/Validators/SongbirdAddressValidator.php
+++ src/BitOasis/Coin/Address/Validators/SongbirdAddressValidator.php
@@ -32,7 +32,7 @@ class SongbirdAddressValidator implements ValidationInterface {
 	 * @throws InvalidAddressException
 	 */
 	public function validateWithExceptions(): bool {
-		if (!preg_match('/^(0x)?[0-9a-f]{40}$/i', $this->address)) {
+		if (!preg_match('/^0x[0-9a-f]{40}$/i', $this->address)) {
 			throw new InvalidAddressException('This is not valid songbird address - ' . $this->address, 0);
 		}
 		return true;
