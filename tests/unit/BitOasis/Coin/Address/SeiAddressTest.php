22753f6706d99043ad5744c5316203fb78d87055
robert.mkrtchyan <robert.mkrtchyan@bitoasis.net>
2025-03-06 18:09:19 +0400

    COV-102: Fix SEI address (Add memo)

--- tests/unit/BitOasis/Coin/Address/SeiAddressTest.php
+++ tests/unit/BitOasis/Coin/Address/SeiAddressTest.php
@@ -14,60 +14,58 @@ use UnitTestUtils;
  */
 class SeiAddressTest extends UnitTest {
 
-	public function providerInvalidAddress() {
+	public function providerInvalidAddress(): array {
 		return [
 			// Invalid: Missing "sei" prefix
-			['0x6c3e4cb2e96bO1f4b866965a91ed4437839a121a'],
-			['notsei1umsz72jtj9n30hkehahhq9mfj5k53apv8s6hsy'],
+			['0x6c3e4cb2e96bO1f4b866965a91ed4437839a121a', 123],
+			['notsei1umsz72jtj9n30hkehahhq9mfj5k53apv8s6hsy', 123],
 			// Invalid: Length is not 32 bytes
-			['sei1umsz72jtj9n30hkehahhq9mfj5k53apv8s6hsyextra'],
-			['notsei1umsz72jtj9n30hkehahhq9mfj5k53apv8s6hsyextra'],
-			['sei1umsz72jtj9n30hkehhhq9mfj5k53apv8s6hsy'],
+			['sei1umsz72jtj9n30hkehahhq9mfj5k53apv8s6hsyextra', 123],
+			['notsei1umsz72jtj9n30hkehahhq9mfj5k53apv8s6hsyextra', 123],
+			['sei1umsz72jtj9n30hkehhhq9mfj5k53apv8s6hsy', null],
 		];
 	}
 
-	public function providerValidate() {
+	public function providerValidate(): array {
 		return [
-			['sei1umsz72jtj9n30hkehahhq9mfj5k53apv8s6hsy'],
-			['sei14y0q9ax6smr32sqt7spxvlsksjw7pyxsqcf9eg'],
-			['sei12p0jcq6ftqa7gpr4mfnm5ymu579yuwpxctxap4'],
-			['sei14n9fhykwk8rk7zln7rzd6uyhm2gzntuw2pv0e9'],
-			['sei13qj8z08uufaj38kffxuwafj5qxfk758hu09czc'],
+			['sei1umsz72jtj9n30hkehahhq9mfj5k53apv8s6hsy', 1234],
+			['sei14y0q9ax6smr32sqt7spxvlsksjw7pyxsqcf9eg', 0],
+			['sei12p0jcq6ftqa7gpr4mfnm5ymu579yuwpxctxap4', 12343],
+			['sei14n9fhykwk8rk7zln7rzd6uyhm2gzntuw2pv0e9', null],
+			['sei13qj8z08uufaj38kffxuwafj5qxfk758hu09czc', 5100],
 		];
 	}
 
 	/**
-	 * @param string $address
 	 * @dataProvider providerInvalidAddress
 	 */
-	public function testInvalidAddress($address) {
+	public function testInvalidAddress(string $address): void {
 		$this->tester->expectThrowable(InvalidAddressException::class, function () use ($address) {
 			$this->createAddress($address);
 		});
 	}
 
 	/**
-	 * @param string $address
 	 * @throws InvalidAddressException
 	 * @dataProvider providerValidate
 	 */
-	public function testAdditionalId($address) {
-		$createdAddress = $this->createAddress($address);
-		$this->assertFalse($createdAddress->supportsAdditionalId());
-		$this->assertNull($createdAddress->getAdditionalIdName());
-		$this->assertNull($createdAddress->getAdditionalId());
+	public function testAdditionalId(string $address, $tag): void {
+		$seiAddress = $this->createAddress($address, $tag);
+		$this->assertTrue($seiAddress->supportsAdditionalId());
+		$this->assertNotNull($seiAddress->getAdditionalIdName());
+		$this->assertEquals($tag, $seiAddress->getAdditionalId());
+		$this->assertEquals($seiAddress->getTag(), $seiAddress->getAdditionalId());
 	}
 
 	/**
-	 * @param string $address
-	 * @return SeiAddress
 	 * @throws InvalidAddressException
 	 */
-	protected function createAddress($address) {
+	protected function createAddress(string $address, $tag = null): SeiAddress {
 		return new SeiAddress(
 			$address,
 			UnitTestUtils::getCryptocurrency(Cryptocurrency::SEI),
-			UnitTestUtils::getCryptocurrencyNetwork(CryptocurrencyNetwork::SEI)
+			UnitTestUtils::getCryptocurrencyNetwork(CryptocurrencyNetwork::SEI),
+			$tag
 		);
 	}
 }
