b147ca69d07e6605b9c676958a6ceb8b207c7c68
Robert <robert.mkrtchyan@bitoasis.net>
2025-03-07 11:25:12 +0400

    COV-102: Fix SEI address (Add memo) (#80)

--- src/BitOasis/Coin/Address/SeiAddress.php
+++ src/BitOasis/Coin/Address/SeiAddress.php
@@ -8,13 +8,22 @@ use BitOasis\Coin\CryptocurrencyAddress;
 /**
  * @author ahmad.yousef <ahmad.yousef@bitoasis.net>
  */
-class SeiAddress extends BaseBech32AddressWithoutTag implements CryptocurrencyAddress {
+class SeiAddress extends BaseBech32AddressWithTag implements CryptocurrencyAddress {
 
 	/**
 	 * @param $address
+	 * @param null $tag
 	 * @return SeiAddressValidator
 	 */
-	protected function createValidator($address): SeiAddressValidator {
-		return new SeiAddressValidator($address);
+	protected function createValidator($address, $tag = null): SeiAddressValidator {
+		return new SeiAddressValidator($address, $tag);
+	}
+
+	public function toString(): string {
+		return 'Address: ' . $this->address . ($this->tag === null ? '' : (', Memo: ' . $this->tag));
+	}
+
+	public static function getClassAdditionalIdName(): string {
+		return 'memo';
 	}
 }
